953e95c781c80c8bc32441f092382c196ae20691
Luis Enrique Campos Mijares <luiscampostorreta@gmail.com>
2018-05-24 11:17:01 -0400

    coloque un comentario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :configure_devise_params, if: :devise_controller?
 
+	# comentario prueba
+
   def configure_devise_params
   	devise_parameter_sanitizer.permit(:sign_up) do |user|
   		user.permit(:name, :email, :password, :password_confirmation)
